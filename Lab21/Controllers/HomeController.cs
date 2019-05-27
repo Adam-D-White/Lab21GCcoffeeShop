544ce88ccf8538027933c92f9151492c4d79ed57
Adam <adwhite.agency@gmail.com>
2019-05-27 15:04:11 -0400

    First commit Lab21

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewUser(NewUserInfo u)
+        {
+            ViewBag.Result = u;
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        //public ActionResult User(string firstName, string lastName, string userName, string email, string password)
+        //{
+
+        //    ViewBag.FirstName = firstName;
+        //    ViewBag.LastName = lastName;
+        //    ViewBag.Username = userName;
+        //    ViewBag.Email = email;
+        //    ViewBag.Username = password;
+
+
+        //    return View();
+        //}
+
+
+    }
+}
---
+++ Lab21/NewUserInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab21
+{
+    public class NewUserInfo
+    {
+
+        public string FirstName
+        { set; get; }
+
+        public string LastName
+        { set; get; }
+
+        public string Username
+        { set; get; }
+
+        public string Email
+        { set; get; }
+
+        public string Password
+        { set; get; }
+
+
+    }
+}
